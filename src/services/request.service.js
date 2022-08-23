2ca103e01339175047cef9fa3b62f3ac91a718ee
DengXinbo <mymail33333@163.com>
2022-08-24 00:54:06 +0800

    更新request

--- src/services/request.service.js
+++ src/services/request.service.js
@@ -2,22 +2,23 @@ import { BASE_URL } from '@/services/environment.service'
 import Toast from '@/wxcomponents/toast/toast'
 import { getToken } from '@/util/index'
 
-const request = function (path, data, method = 'POST') {
+const request = function (path, data = {}, method = 'POST') {
   return new Promise((resolve, reject) => {
     let token = getToken() || ''
     let url = BASE_URL + `${path}`
+    if (token) data.token = token
     uni.request({
       url,
       method,
       data,
       header: {
-        cookie: token
+        // cookie: token
       },
       success: (res) => {
-        if (res.statusCode >= 200 && res.statusCode < 300) {
+        if (res.data.errno == 1 && res.statusCode >= 200 && res.statusCode < 300) {
           resolve(res.data)
         } else if (res.statusCode == 401) {
-          Toast('登录失效，请重试!');
+          Toast('登录失效，请重试!')
         } else if (res.statusCode == 403) {
           Toast((res.data.error && res.data.error.message) || '登陆失败!')
           reject(res)
