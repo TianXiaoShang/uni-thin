ddf82b5d321a857c6a390d5f302be4b76cdf885f
DengXinbo <mymail33333@163.com>
2022-09-03 16:33:52 +0800

    更新store

--- src/store/index.js
+++ src/store/index.js
@@ -1,42 +1,52 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import request from '@/services/request.service';
-import { setToken, clearToken,setTokenTemporary } from '@/util/storage';
+import request from '@/services/request.service'
+import { setToken, clearToken, setTokenTemporary } from '@/util/storage'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    state: {
-        loginStatus: false, // 获取登陆状态
-        token: null,
-        logining: false,
-        userInfo: {}, // 储存来自weixin授权的用户信息（头像信息等）
+  state: {
+    loginStatus: false, // 获取登陆状态
+    token: null,
+    logining: false,
+    userInfo: {}, // 储存来自weixin授权的用户信息（头像信息等）
+    group: []
+  },
+  getters: {
+    loginStatus: (state) => state.loginStatus,
+    userInfo: (state) => state.userInfo,
+    token: (state) => state.token,
+    openId: (state) => state.userInfo.openid,
+    group: (state) => state.group,
+    groupMap: (state) =>
+      state.group?.reduce((obj, d) => {
+        obj[d.id] = d
+        return obj
+      }, {})
+  },
+  mutations: {
+    // 登陆，更新状态
+    LOGIN(state, value) {
+      state.loginStatus = value
     },
-    getters: {
-        loginStatus: state => state.loginStatus,
-        userInfo: state => state.userInfo,
-        token: state => state.token,
-        openId: state => state.userInfo.openid,
+    SET_TOKEN(state, value) {
+      state.token = value
+      setToken(value)
     },
-    mutations: {
-        // 登陆，更新状态
-        LOGIN(state, value) {
-            state.loginStatus = value;
-        },
-        SET_TOKEN(state, value){
-            state.token = value;
-            setToken(value);
-        },
-        // 登出
-        LOGOUT(state) {
-            state.loginStatus = false;
-            state.token = null;
-            clearToken();
-        },
-        UPDATE_USERINFO(state, value) {
-            state.userInfo = value;
-        },
+    // 登出
+    LOGOUT(state) {
+      state.loginStatus = false
+      state.token = null
+      clearToken()
     },
+    UPDATE_USERINFO(state, value) {
+      state.userInfo = value
+    },
+    UPDATE_GROUP(state, value) {
+      state.group = value
+    }
+  }
 })
 
-export default store;
+export default store
